902df1c93c28923a1a8505fbd0be6eb8411116fc
Mio Wong <mio.wong@swivelsoftware.com>
2022-07-08 12:52:48 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Chat Widget Everywhere
+
+Embed chat widget into anywhere that runs HTML and JS.
+
+## Installation
+
+Place a div with id `chat-container` into where you want it to be with
+```html
+<div id="chat-container"></div>
+```
+
+Include a copy of `swivel-chat-widget-min.js` and helper script at the end of html page with
+  ```html
+  <script type="text/javascript" src="./swivel-chat-widget.min.js"></script>
+  <script type="text/javascript" src="./create-chat-widget.js"></script>
+  ```
+
+Create additional `<script>` block and run the following
+```html
+<script>
+/**
+ * Main widget create function
+ * @param {string} username Username (email) of the user
+ * @param {string} passwordHash SHA3-256 Hashed password
+ */
+    createWidget("mio9+chat109@swivelsoftware.com", "6aa3665554665c26fe82ae63f9997eb8b7930f9a6dd943a21ea8ff6da13333ae")
+</script>
+```
+
+## Usage Example
+```html
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+
+  <body>
+    <!-- Embed chat widget button -->
+    <p>Some other elements</p>
+    <div id="chat-container"></div> 
+    <p>Some more other elements</p>
+  </body>
+
+  <!-- Include widget and helper script -->
+  <script type="text/javascript" src="https://chat-uat.swivelsoftware.asia/v2/widgets/swivel-chat-widget.js"></script>
+  <script type="text/javascript" src="./create-chat-widget.js"></script>
+
+  <!-- Create the widget -->
+  <script>
+    createWidget("mio9+chat109@swivelsoftware.com", "ed46fbf7cbd33c61df4a97fc3c444393f838ecbdd438e34d903f4d0a6854ee39") 
+  </script>
+</html>
+
+
+```
---
+++ create-chat-widget.js
@@ -0,0 +1,71 @@
+async function _login(username, passwordHash) {
+  const response = await fetch('https://auth.swivelsoftware.asia/auth/local/login',{
+    method: 'POST',
+    headers:{
+      'x-system': '360uat',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username, password: passwordHash, rememberMe: false})
+  });
+  const data = await response.json();
+  return data['accessToken']
+}
+
+function _mountWidget(token){
+  const widgetContainer = document.getElementById("chat-container"); // div id
+  const widget = document.createElement("swivel-chat-widget");
+  widget.setAttribute("id", "swivel-chat-widget");
+  widget.setAttribute("name", JSON.stringify(["Main"])); // enter the widget name
+  // widget.setAttribute('style', widgetStyle)
+  widget.setAttribute(
+    "token",
+    token
+  ); // token
+  widget.setAttribute("system", "360uat"); // system
+  // if (uat) {
+  widget.setAttribute("uat", "true");
+  // }
+  widget.setAttribute(
+    "propdata",
+    JSON.stringify({
+      full: false,
+      button: true,
+      right: false,
+      system: "360uat",
+      entityType: "customer",
+      entityKey: "DEV",
+      entityReferenceKey: "Development Team",
+      hasPublicChatroom: true,
+      extraChatrooms: [],
+      maxShowChatroom: 4,
+      leftOffset: 0,
+      rightOffset: 0,
+      // propdata
+    })
+  );
+  widget.setAttribute("injectCss", widgetContainer.id);
+  const shadowRootStyle = document.createElement("style");
+  shadowRootStyle.innerHTML = "";
+  widgetContainer.appendChild(widget);
+  if (widget.shadowRoot) {
+    console.log(shadowRootStyle);
+    widget.shadowRoot.appendChild(shadowRootStyle);
+  }
+  document.body.addEventListener("swivel-widget-loaded", () => {
+    if (widget.shadowRoot) {
+      widget.shadowRoot.appendChild(shadowRootStyle);
+    }
+  });
+}
+
+/**
+ * Main widget create function
+ * @param {string} username Username (email) of the user
+ * @param {string} passwordHash SHA3-256 Hashed password
+ */
+async function createWidget(username, passwordHash) {
+  const token = await _login(username, passwordHash)
+  _mountWidget(token)
+}
+
+// main();
